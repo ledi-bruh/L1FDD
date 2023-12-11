d228612f065cb480d0e42a2cc195cf3e684d66a9
Andrey Podovinov <andrey.podovinov@iqtek.ru>
2023-12-12 01:54:04 +0600

    added colors and string

---
+++ lib/app/resources/colors.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+abstract class AppColors {
+  static const Color primary = Colors.white;
+  static const Color cardPrimary = Colors.white;
+  static const Color textPrimary = Colors.black;
+  static final Color textSecondary = Colors.black.withOpacity(0.55);
+  static final Color divider = Colors.black.withOpacity(0.12);
+  static const Color tabBrand = Color(0xFF068441);
+  static const Color iconBrand = Color(0xFF08A652);
+  static final Color iconSecondary = Colors.black.withOpacity(0.32);
+  static final Color chipsBrandEnabled = Colors.black.withOpacity(0.08);
+}
---
+++ lib/app/resources/strings.dart
@@ -0,0 +1,8 @@
+abstract class Strings {
+  static const String appTitle = "Sbercraft";
+  static const String userName = "Екатерина";
+  static const String profile = "Профиль";
+  static const String settings = "Настройки";
+  static const String subsTitle = "У вас подключено";
+  static const String subsDescription = "Подписки, автоплатежи и сервисы, на которые вы подписались";
+}
