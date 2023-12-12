d4da081898f9684f6614bf0d1658f87671ff020d
Andrey Podovinov <andrey.podovinov@iqtek.ru>
2023-12-13 01:52:52 +0600

    added default value on settings page

---
+++ lib/app/pages/settings_page.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+import 'package:fall_2023_project/app/resources/strings.dart';
+
+class SettingsPage extends StatelessWidget {
+  const SettingsPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Text(Strings.inDev);
+  }
+}
